1fe5461258f97040a58e4fd79d38edf5e206b0cf
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-08-15 00:19:22 -0400

    Added function to print times using callback waterfall

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 
-// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const printTimes = function(passTimes) {
+  for (const key in passTimes) {
+    const time = passTimes[key].risetime;
+    const date = new Date(time * 1000);
+    const duration = passTimes[key].duration;
+    console.log((`Next pass at ${date} for ${duration} seconds!`));
+  }
+};
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+const { nextISSTimesForMyLocation } = require('./iss');
 
-//   console.log('It worked! Returned IP:' , ip);
-// });
-
-// fetchCoordsByIP('99.240.219.229', (error, coordinates) => {
-//   if (error) {
-//     console.log(error, "It didn't work!");
-//     return;
-//   }
-
-//   console.log("It worked", coordinates);
-// });
-
-// fetchISSFlyOverTimes(("43.653226","-79.3831843"), (error, result) =>  {
-//   if (error) {
-//     console.log(error, "It didn't work!");
-//     return;
-//   }
-
-//   console.log('It worked! Returned flyover times:', result);
-// });
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printTimes(passTimes);
+});
